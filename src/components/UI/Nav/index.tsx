1be569861699e0b6e65519652f7a939da8036bf5
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-01-09 22:36:03 -0500

    [feat] screen상태 전환 구현
    - nav아이콘 클릭 시 screen상태가 변경
    - 상태에 따라 ControlBox렌더링

--- src/components/UI/Nav/index.tsx
+++ src/components/UI/Nav/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { ScreenType } from '../../template/mainTemplate';
 import Icon from '../Icon';
 import * as Style from './styled';
 
-function Nav() {
+interface NavProps {
+  screen: ScreenType;
+  changeScreen: (screen: ScreenType) => void;
+}
+
+function Nav({ screen, changeScreen }: NavProps) {
   return (
     <Style.Container>
-      <Style.IconWrapper>
-        <Icon icon="home" color="#ccc" width="70%" />
+      <Style.IconWrapper onClick={() => changeScreen('home')}>
+        <Icon icon="home" color={screen === 'home' ? '#9e7715' : '#ccc'} width="70%" />
       </Style.IconWrapper>
-      <Style.IconWrapper>
-        <Icon icon="clock" color="#ccc" width="70%" />
+      <Style.IconWrapper onClick={() => changeScreen('date')}>
+        <Icon icon="clock" color={screen === 'date' ? '#9e7715' : '#ccc'} width="70%" />
       </Style.IconWrapper>
-      <Style.IconWrapper>
-        <Icon icon="compare" color="#ccc" width="70%" />
+      <Style.IconWrapper onClick={() => changeScreen('compare')}>
+        <Icon icon="compare" color={screen === 'compare' ? '#9e7715' : '#ccc'} width="70%" />
       </Style.IconWrapper>
     </Style.Container>
   );
--- src/components/template/mainTemplate/index.tsx
+++ src/components/template/mainTemplate/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ControlBox from '../../UI/ControlBox';
 import Header from '../../UI/Header';
 import Map from '../../UI/Map';
 import Nav from '../../UI/Nav';
 
+export type ScreenType = 'home' | 'date' | 'compare';
+
 function MainTemplate() {
+  const [screen, setScreen] = useState<ScreenType>('home');
+
+  const changeScreen = (screen: ScreenType) => {
+    setScreen(screen);
+  };
+
   return (
     <>
       <Header />
-      <Nav />
-      <ControlBox />
+      <Nav screen={screen} changeScreen={changeScreen} />
+      {screen === 'date' && <ControlBox />}
       <Map />
     </>
   );
