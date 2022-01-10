2690f427cc7cc85f0321856cd55ebf203501c5e1
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-01-09 20:29:01 -0500

    [feat] ControlBox UI Component 스타일링
    - 기본적인 스타일링

--- src/components/UI/ControlBox/index.tsx
+++ src/components/UI/ControlBox/index.tsx
@@ -2,7 +2,47 @@ import React from 'react';
 import * as Style from './styled';
 
 function ControlBox() {
-  return <Style.Container />;
+  const dummyTrees = [
+    { color: 'red', name: 'maple' },
+    { color: 'blue', name: 'ash' },
+  ];
+
+  return (
+    <Style.Container>
+      <Style.Title>CONTROL</Style.Title>
+      <Style.SubTitle>Date</Style.SubTitle>
+      <Style.DateWrapper>
+        <Style.InputBox checked id="checkbox" type="checkbox" />
+        <Style.InputLabel htmlFor="checkbox">all</Style.InputLabel>
+      </Style.DateWrapper>
+      <Style.DateWrapper>
+        <Style.InputText type="date" />
+        ~
+        <Style.InputText type="date" />
+      </Style.DateWrapper>
+      <Style.SubTitle>Species</Style.SubTitle>
+      <Style.SpeciesWrapper>
+        <Style.SpeciesSelect>
+          {dummyTrees.map((item) => (
+            <Style.SpeciesOption key={item.name} value={item.name}>
+              {item.name}
+            </Style.SpeciesOption>
+          ))}
+          <Style.SpeciesOption />
+        </Style.SpeciesSelect>
+        <Style.SpeciesList>
+          {dummyTrees.map((item) => (
+            <Style.Species key={item.name}>
+              <Style.SpeciesCircle color={item.color} />
+              <Style.SpeciesTitle>{item.name}</Style.SpeciesTitle>
+            </Style.Species>
+          ))}
+        </Style.SpeciesList>
+      </Style.SpeciesWrapper>
+      <Style.SubTitle>Result</Style.SubTitle>
+      <Style.ResultWrapper />
+    </Style.Container>
+  );
 }
 
 export default ControlBox;
--- src/components/UI/ControlBox/styled.ts
+++ src/components/UI/ControlBox/styled.ts
@@ -1,13 +1,91 @@
 import styled from '@emotion/styled';
 
+interface SpeciesCircleProps {
+  color: string;
+}
+
 export const Container = styled.div`
   position: fixed;
   top: 8rem;
   left: 8rem;
   width: 40rem;
-  height: 40rem;
+  height: 36rem;
   z-index: 1000;
   padding: 1.2rem;
   border: 1px solid #9e7715;
   background-color: #ffffff;
 `;
+
+export const Title = styled.h2`
+  margin: 0;
+  font-size: 2.4rem;
+`;
+
+export const SubTitle = styled.h3`
+  margin: 0;
+  margin-top: 2rem;
+  font-size: 1.6rem;
+`;
+
+export const DateWrapper = styled.div`
+  gap: 1rem;
+  margin: 0.7rem 0;
+  display: flex;
+  align-items: center;
+`;
+
+export const InputBox = styled.input`
+  width: 15px;
+  height: 15px;
+  margin: 0;
+`;
+
+export const InputLabel = styled.label`
+  font-size: 1.4rem;
+`;
+
+export const InputText = styled.input`
+  height: 2.4rem;
+  border: 1px solid #9e7715;
+`;
+
+export const SpeciesWrapper = styled.div`
+  margin: 1rem 0;
+`;
+
+export const SpeciesSelect = styled.select`
+  width: 10rem;
+  height: 2.4rem;
+  border: 1px solid #9e7715;
+`;
+
+export const SpeciesOption = styled.option``;
+
+export const SpeciesList = styled.ul`
+  padding: 0;
+  margin-top: 1rem;
+
+  gap: 1.5rem;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Species = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+export const SpeciesCircle = styled.div<SpeciesCircleProps>`
+  width: 1rem;
+  height: 1rem;
+  margin-right: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: ${(props) => props.color};
+`;
+
+export const SpeciesTitle = styled.h4`
+  margin: 0;
+  font-size: 1.4rem;
+`;
+
+export const ResultWrapper = styled.div``;
